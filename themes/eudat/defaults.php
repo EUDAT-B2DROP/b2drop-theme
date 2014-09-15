dd9bcff7848a2540deb4bb54d179d90bcac8162b
bvonstvieth <b.von.st.vieth@fz-juelich.de>
2014-09-15 14:01:49 +0200

    working copy with more logos and a modified share-email, still problems with Safari svg rendering.

--- themes/eudat/defaults.php
+++ themes/eudat/defaults.php
@@ -5,7 +5,9 @@ class OC_Theme {
 
     function __construct() {
         $this->themeName = "B2DROP";
+        $this->slogan = "Sync and Exchange Research Data";
         $this->themeFooter = "<b> New to EUDAT B2DROP? </b> <a href='https://b2drop.fz-juelich.de/pwm'> <br/><i>Create a test account!<i></a>";
+        $this->baseUrl = "https://b2drop.eudat.eu";
     }
    
     public function getTitle() {
@@ -16,4 +18,15 @@ class OC_Theme {
         return $this->themeFooter;
     }
 
+    public function getName() {
+        return $this->themeName;
+    }
+
+    public function getSlogan() {
+        return $this->slogan;
+    }
+
+    public function getBaseUrl() {
+        return $this->baseUrl;
+    }
 }
