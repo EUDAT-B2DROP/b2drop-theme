3f8762c9a080dd0be3a4c2ebb4e60e5cf1824d58
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-05-20 08:02:09 +0200

    static footer for login page, getLongFooter set to empty string.

--- defaults.php
+++ defaults.php
@@ -3,36 +3,7 @@ class OC_Theme {
     function __construct() {
         $this->themeName = "b2drop";
         $this->slogan = "Sync and Exchange Research Data";
-        $this->themeCompleteFooter = '
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-6 col-md-6">
-                    <p><img width="45" height="31" src="/themes/b2drop/core/img/flag-ce.jpg"
-                            style="float:left; margin-right:10px;"/> EUDAT receives funding from the European Union’s
-                        Horizon 2020 research and innovation programme under grant agreement No. 654065. <a href="#">Legal
-                            Notice</a>.</p>
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-6 text-right">
-                    <nav class="navbar navbar-footer">
-                        <ul id="menu" class="nav navbar-nav navbar-right pull-right">
-                            <li><a href="https://b2drop.eudat.eu/ToU.pdf">Terms of Use</a></li>
-                            <li><a target="_blank" href="https://eudat.eu/what-eudat">About EUDAT</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
-        ';
-        $this->themeFooter = '
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-6 col-md-6">
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-6 text-right">
-                </div>
-            </div>
-        </div>
-        ';
+        $this->themeFooter = '';
         $this->baseUrl = "https://b2drop.eudat.eu";
     }
    
@@ -42,10 +13,6 @@ class OC_Theme {
 
     public function getLongFooter() {
         return $this->themeFooter;
-    }
-
-    public function getCompleteFooter() {
-        return $this->themeCompleteFooter;
     }
 
     public function getName() {
