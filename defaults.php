5965e3ba5d2e9ef0e580fca48988446c7dd15809
Mikael Karlsson <i8myshoes@gmail.com>
2015-11-06 15:42:24 +0200

    Change theme info

--- defaults.php
+++ defaults.php
@@ -1,10 +1,10 @@
 <?php
 class OC_Theme {
     function __construct() {
-        $this->themeName = "b2drop";
+        $this->themeName = "B2DROP Finland";
         $this->slogan = "Sync and Exchange Research Data";
-        $this->themeFooter = '';
-        $this->baseUrl = "https://b2drop.eudat.eu";
+        $this->themeFooter = 'Provided by CSC - IT Center for Science, Finland';
+        $this->baseUrl = "https://fi.b2drop.eudat.eu";
         $this->iTunesAppId = "543672169";
     }
 
